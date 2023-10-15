aec05c2c5ecf88804af9bebdfe26b4bcb5bf54cd
xalessandrox <formicale@hotmail.com>
2023-10-15 10:36:12 +0200

    start commit

---
+++ src/main/java/com/sandro/catering_demo/CateringDemoApplication.java
@@ -0,0 +1,13 @@
+package com.sandro.catering_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CateringDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CateringDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sandro/catering_demo/CateringDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandro.catering_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CateringDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
